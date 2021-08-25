162571c740d5e006b01c153100fb4f516b8119f3
AlonsoH27 <33145379@uaz.edu.mx>
2021-08-25 00:28:00 -0500

    p02 Area de un circulo

--- p02-areaCirculo/Program.cs
+++ p02-areaCirculo/Program.cs
@@ -6,7 +6,13 @@ namespace p02_areaCirculo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            float r=0;
+            double a=0;
+            Console.WriteLine("Calcula el area de un circulo");
+            Console.WriteLine("Dame el radio");
+            r = float.Parse(Console.ReadLine());
+            a = Math.PI * Math.Pow(r,2);
+            Console.WriteLine($"El area del circulo es de: {a}");
         }
     }
 }
