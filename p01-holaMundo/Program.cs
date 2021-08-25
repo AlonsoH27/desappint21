f704e7199f466a482706d427549c9ac02b5d51c2
AlonsoH27 <33145379@uaz.edu.mx>
2021-08-25 00:15:38 -0500

    p01-holaMundo

---
+++ p01-holaMundo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace p01_holaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            DateTime fecha = DateTime.Now;
+            Console.WriteLine("¡Hola Mundo! \n");
+            Console.WriteLine("Dame tu nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine($"\n{nombre} bienvenido al c#");
+            Console.WriteLine($"Hoy es: {fecha:d} y son las {fecha:t}");
+            Console.WriteLine($"\nHasta pronto");
+
+        }
+    }
+}
---
+++ p02-areaCirculo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace p02_areaCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
