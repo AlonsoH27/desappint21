f31be66bd9e9a716334be6dc7d74c11c7dde3668
AlonsoH27 <33145379@uaz.edu.mx>
2021-08-25 00:33:18 -0500

    p03 Area de un triangulo

---
+++ p03-areaTriangulo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace p02_areaTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float b, al, a;
+            b = al = a = 0;
+            Console.WriteLine("Calculando el area de un triangulo\n");
+            Console.WriteLine("Dame la base");
+            b = float.Parse(Console.ReadLine());
+            Console.WriteLine("Dame la altura");
+            al = float.Parse(Console.ReadLine());
+            a = (b * al) / 2;
+            Console.WriteLine($"El area del triangulo es de: {a}");
+
+
+        }
+    }
+}
