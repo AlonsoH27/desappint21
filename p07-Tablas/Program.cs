bd47e66cadf9e1e4303985968829a855a3e5bcc2
AlonsoH27 <33145379@uaz.edu.mx>
2021-09-03 18:36:42 -0500

    p07 Tablas

---
+++ p07-Tablas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace p07_Tablas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int multiplicador, final;
+            string linea, tope;
+            Console.Write("¿Que tabla de multiplicar? :  ");
+            linea = Console.ReadLine();
+            Console.Write("¿Hasta que numero?: ");
+            tope= Console.ReadLine();
+            multiplicador = int.Parse(linea);
+            final = int.Parse(tope);
+            for(int i=1; i<=final; i++ ) {              
+                    Console.Write(i+" x "+multiplicador+" = "+i*multiplicador+"\n");               
+            }           
+        }
+    }
+}
