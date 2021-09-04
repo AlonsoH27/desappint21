0b50c1964bd77a7abfae0bf2c826f1d6eaa0d63a
AlonsoH27 <33145379@uaz.edu.mx>
2021-09-03 19:15:45 -0500

    p13 Vectores multiplica

--- p13-vectoresmultiplica/Program.cs
+++ p13-vectoresmultiplica/Program.cs
@@ -1,23 +1,32 @@
 ﻿using System;
 
-namespace p12_vectorinvertido
+namespace p13_vectoresmultiplica
 {
     class Program
     {
         static void Main(string[] args)
         {
-            const int TAM=16;
+            const int TAM=20;
             int [] A = new int [TAM];
             int [] B = new int [TAM];
-            
+            int [] C = new int [TAM];
+            int [] D = new int [TAM];
             Random rnd = new Random();
 
             for(int i=0; i<A.Length; i++){
                 A[i] = rnd.Next(1,100);
-                B[15-i] = A[i];
+                for(int j=0; j<B.Length; j++){
+                    B[j] = rnd.Next(1,100);
+                    D[19-j] = B[j]; 
+                    C[i] = A[i]*D[j];
+                }
+                
             }
+           
             Console.WriteLine("\n Valores de A "); imprime(A);
             Console.WriteLine("\n Valores de B "); imprime(B);
+            Console.WriteLine("\n Valores invertidos de B "); imprime(D);
+            Console.WriteLine("\n Valores de C "); imprime(C);
 
         }
         static void imprime(int[] v) {
